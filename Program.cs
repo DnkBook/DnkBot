37ac31a4ada0118d19abcbd6524675ad59500316
Cyl18 <cyl18a@gmail.com>
2024-11-22 14:19:52 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Sisters.WudiLib;
+
+Console.WriteLine("Hello, World!");
+var group = 954738468;
+var qq = new HttpApiClient();
+qq.ApiAddress = "http://192.168.0.56:5700/";
+
+SendMessage("喵");
+
+void SendMessage(string s)
+{
+    qq.SendGroupMessageAsync(group, s).Wait();
+}
