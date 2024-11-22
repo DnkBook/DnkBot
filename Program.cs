2e1ed08d656f0bee07530f93523cc745d8aa8f0f
Cyl18 <cyl18a@gmail.com>
2024-11-22 17:15:08 +0800

    ok!

--- Program.cs
+++ Program.cs
@@ -1,12 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using cqhttp.Cyan.Clients;
-using cqhttp.Cyan.Enums;
 using GammaLibrary.Extensions;
 using Sisters.WudiLib;
+using Sisters.WudiLib.Posts;
 using Message = Sisters.WudiLib.Posts.Message;
 
-var group = 954738468;
 var qq = new HttpApiClient();
 qq.ApiAddress = "http://192.168.0.234:10000/";
 var listener = new Sisters.WudiLib.WebSocket.CqHttpWebSocketEvent("ws://192.168.0.234:10001/");
@@ -14,9 +12,9 @@ var listener = new Sisters.WudiLib.WebSocket.CqHttpWebSocketEvent("ws://192.168.
 listener.ApiClient = qq;
 listener.MessageEvent += (api, message) =>
 {
+    var msg = (GroupMessage)message;
     if (message.MessageType is Message.GroupType && message.Source.UserId is 1538757052 or 920059839)
     {
-
         var text = message.Content.Text;
         if (text.Contains("呕")
             || text.Contains("哎")
@@ -29,7 +27,7 @@ listener.MessageEvent += (api, message) =>
             || text.ToLower().Contains("omc")
             )
         {
-            SendDnkFun();
+            SendDnkFun(msg.GroupId);
         }
 
     }
@@ -39,18 +37,14 @@ await listener.StartListen(CancellationToken.None);
 
 Thread.CurrentThread.Join();
 
-void SendDnkFun()
+void SendDnkFun(long group)
 {
     var files = Directory.GetFiles("../../../dnkFuns/dnkBook", "*.*", SearchOption.AllDirectories).Where(x =>
         Path.GetFileName(x).EndsWith(".png") || Path.GetFileName(x).EndsWith(".jpg"));
-    SendImage(files.PickOne());
-}
-void SendMessage(string s)
-{
-    qq.SendGroupMessageAsync(group, s).Wait();
+    SendImage(group,files.PickOne());
 }
 
-void SendImage(string path)
+void SendImage(long group, string path)
 {
     qq.SendGroupMessageAsync(group, SendingMessage.LocalImage(path, true)).Wait();
 }
